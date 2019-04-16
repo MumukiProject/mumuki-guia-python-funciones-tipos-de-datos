45549873228de52641d322fd27788ebc4c0fa3d5
franco@mumuki.org <franco@mumuki.org>
2019-04-16 21:01:21 +0000

    Mumuki Export on 2019-04-16 21:01:21 +0000

--- 00004_¿Es par_/test.py
+++ 00004_¿Es par_/test.py
@@ -1,16 +1,16 @@
   def test_es_par_2_es_verdadero(self):
     self.assertTrue(es_par(2))
-
+  
   def test_es_par_8_es_verdadero(self):
     self.assertTrue(es_par(8))
-
+  
   def test_es_par_9890_es_verdadero(self):
     self.assertTrue(es_par(9890))
-
+  
   def test_es_par_1_es_falso(self):
     self.assertFalse(es_par(1))
-
+  
   def test_es_par_879_es_falso(self):
     self.assertFalse(es_par(879))
-
-
+  
+  
