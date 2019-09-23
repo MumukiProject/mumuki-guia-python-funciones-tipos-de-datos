a69873fb83f6b49a3a1f388ddd683993d842ddb6
franco@mumuki.org <franco@mumuki.org>
2019-09-23 14:04:09 +0000

    Mumuki Export on 2019-09-23 14:04:09 +0000

--- 00013_Palabras, sólo palabras/test.py
+++ 00013_Palabras, sólo palabras/test.py
@@ -5,10 +5,10 @@
   def test_el_domingo_es_fin_de_semana(self):
     self.assertTrue(es_fin_de_semana("domingo"))
 
-  def test_el_lunes_NO_es_fin_de_semana(self):
+  def test_el_lunes_no_es_fin_de_semana(self):
     self.assertFalse(es_fin_de_semana("lunes"))
 
-  def test_el_viernes_NO_es_fin_de_semana(self):
+  def test_el_viernes_no_es_fin_de_semana(self):
     self.assertFalse(es_fin_de_semana("viernes"))
 
 
