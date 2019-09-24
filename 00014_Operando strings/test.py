b3654cbc24c6f4168284d863185307f08805f0d5
gustavo@mumuki.org <gustavo@mumuki.org>
2019-09-24 20:04:23 +0000

    Mumuki Export on 2019-09-24 20:04:23 +0000

--- 00014_Operando strings/test.py
+++ 00014_Operando strings/test.py
@@ -7,13 +7,3 @@
 
   def test_longitud_nombre_completo_Juana_Azurduy(self):
     self.assertEqual(longitud_nombre_completo("Juana", "Azurduy"), 13)
-
-  def test_se_contempla_el_espacio_entre_nombre_y_apellido(self):
-    try:
-      longitud = longitud_nombre_completo("Gus", "Trucco")
-    except:
-      longitud = 10
-    
-    if(longitud == 9):
-      self.fail("Te faltó contar el espacio")
-  
